bec87a9e635a48d6ef9d824d5fa8fa7adcc66cc8
TravisCornelius <travis.cornelius@firespring.com>
2016-09-02 15:42:23 -0500

    init

---
+++ Assets/_Project/Scripts/GhostMode.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostMode : MonoBehaviour {
+
+	Shader originalShader;
+	Shader ghostShader;
+
+	Renderer rend;
+    SkinnedMeshRenderer meshRend;
+    public bool dontRender = false;
+
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+        meshRend = GetComponent<SkinnedMeshRenderer>();
+        originalShader = rend.material.shader;
+        Debug.Log(originalShader);
+		ghostShader = Shader.Find ("Ciconia Studio/Effects/Ghost/Old version(1.2)/Ghost Animated Details");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerScript.ghostMode) {
+            if (dontRender)
+            {
+                meshRend.enabled = false;
+            } else
+            {
+                rend.material.shader = ghostShader;
+            }
+			
+		} else {
+            meshRend.enabled = true;
+			rend.material.shader = originalShader;
+		}
+	}
+}
---
+++ Assets/_Project/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿ using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour {
+
+	public static bool ghostMode = false;
+    public bool viewGhost = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //viewGhost = ghostMode;
+        ghostMode = viewGhost;
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+        Debug.Log(col.gameObject.name);
+		if (col.gameObject.name == "ZombiePortal")
+		{
+            SceneManager.LoadScene("Zombie");
+		} 
+		if(col.gameObject.name == "NormalPortal")
+		{
+			ghostMode = false;
+		}
+		if(col.gameObject.name == "GhostPortal")
+		{
+			ghostMode = true;
+		}
+	}
+}
