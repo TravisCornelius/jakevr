18fa707e6177984ceefc1b367fa2332876d66229
TravisCornelius <travis.cornelius@firespring.com>
2016-09-06 15:06:10 -0500

    fatal2

---
+++ Assets/VRTK/Examples/Resources/Scripts/ControlReactor_Code.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class ControlReactor_Code : MonoBehaviour
+{
+    public TextMesh go;
+    public GameObject slider1;
+    public GameObject slider2;
+    public GameObject slider3;
+    public GameObject cube;
+
+    private void Start()
+    {
+        slider1 = GameObject.Find("Slider1");
+        slider2 = GameObject.Find("Slider2");
+        slider3 = GameObject.Find("Slider3");
+
+        cube = GameObject.Find("Cube");
+        cube.GetComponent<Renderer>().enabled = false;
+    }
+
+    private void Update()
+    {
+        SliderChange(slider1.GetComponent<VRTK_Control>().GetValue(), slider2.GetComponent<VRTK_Control>().GetValue(), slider3.GetComponent<VRTK_Control>().GetValue());
+    }
+
+    private void SliderChange(float value1, float value2, float value3)
+    {
+
+        if (value1 == 9)
+        {
+            if (value2 == 3)
+            {
+                if (value3 == 5)
+                {
+                    cube.GetComponent<Renderer>().enabled = true;
+                    go.text = "4852";
+                }
+                else
+                {
+                    cube.GetComponent<Renderer>().enabled = false;
+                    go.text = "";
+                }
+            }
+        }
+    }
+
+}
