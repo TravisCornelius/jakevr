f9fbae8e87a5e0b482e0f1cf217052afe1305230
Travis Cornelius <travis@mybringback.com>
2016-09-08 17:25:46 -0500

    audio updates

--- Assets/playCollisionSound.cs
+++ Assets/playCollisionSound.cs
@@ -4,32 +4,41 @@ using System.Collections;
 
 public class playCollisionSound : MonoBehaviour {
 
-    AudioSource source;
+    /*
+     * 
+     * Make sure the last AudioSource in the list is empty if you don't want to
+     * override sounds.
+     * 
+     */
+
+    AudioSource[] sources;
+    AudioSource lastSource;
     public AudioClip heavyForceClip;
     public AudioClip mediumForceClip;
     public AudioClip lightForceClip;
 
     // Use this for initialization
     void Start () {
-        source = GetComponent<AudioSource>();
+        sources = GetComponents<AudioSource>();
+        lastSource = sources[sources.Length-1];
 
-	}
+    }
 	
     void OnCollisionEnter(Collision collision)
     {
         float force = collision.relativeVelocity.magnitude;
         if (force > 2 && force <= 5)
         {
-            source.clip = lightForceClip;
-            source.Play();
+            lastSource.clip = lightForceClip;
+            lastSource.Play();
         } else if (force > 5 && force <= 10)
         {
-            source.clip = mediumForceClip;
-            source.Play();
+            lastSource.clip = mediumForceClip;
+            lastSource.Play();
         } else if (force > 10)
         {
-            source.clip = lightForceClip;
-            source.Play();
+            lastSource.clip = lightForceClip;
+            lastSource.Play();
         }
 
 
