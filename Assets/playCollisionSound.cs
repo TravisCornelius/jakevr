030b106dfaaedbde87b79028897dfc23ba218539
Travis Cornelius <travis@mybringback.com>
2016-09-08 16:46:41 -0500

    audio stuff

---
+++ Assets/playCollisionSound.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+
+public class playCollisionSound : MonoBehaviour {
+
+    AudioSource source;
+    public AudioClip heavyForceClip;
+    public AudioClip mediumForceClip;
+    public AudioClip lightForceClip;
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+
+	}
+	
+    void OnCollisionEnter(Collision collision)
+    {
+        float force = collision.relativeVelocity.magnitude;
+        if (force > 2 && force <= 5)
+        {
+            source.clip = lightForceClip;
+            source.Play();
+        } else if (force > 5 && force <= 10)
+        {
+            source.clip = mediumForceClip;
+            source.Play();
+        } else if (force > 10)
+        {
+            source.clip = lightForceClip;
+            source.Play();
+        }
+
+
+    }
+}
