6b51c942b05b6da2ec027af25550c53f1e98d721
Travis Cornelius <travis@mybringback.com>
2016-09-10 14:52:35 -0500

    update

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -5,6 +5,9 @@ public class SceneController : MonoBehaviour {
     Light[] lights;
     public static bool lightsOn = false;
     private bool lightsOnTrigger = true;
+    public static float timer = 7 * 60 * 1000;
+    public static bool gameOver = false;
+
 	// Use this for initialization
 	void Start () {
         lights = GetComponentsInChildren<Light>();
@@ -16,6 +19,14 @@ public class SceneController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+ 
+            
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                gameOver = true;
+            }
+      
         if (lightsOnTrigger && lightsOn)
         {
             foreach (Light l in lights)
---
+++ Assets/watch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class watch : MonoBehaviour {
+
+    TextMesh mesh;          
+	// Use this for initialization
+	void Start () {
+        mesh = GetComponent<TextMesh>();
+                
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mesh.text = "" + SceneController.timer;
+	}
+}
