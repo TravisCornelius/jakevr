1fce94b970f3b8a5b9f73a167f76f3ff88b63037
TravisCornelius <travis.cornelius@firespring.com>
2016-09-14 16:03:14 -0500

    menu mods

---
+++ Assets/_Project/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using VRTK;
+
+public class MenuController : MonoBehaviour
+{
+
+    public void Button_Hospital_Horror()
+    {
+        Debug.Log("Hospital Horror Button Clicked");
+        
+    }
+
+
+}
