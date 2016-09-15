cd032e8e2521c57621092adf8228ca82c8786063
Travis Cornelius <travis@mybringback.com>
2016-09-15 18:59:39 -0500

    credit script

---
+++ Assets/HauntedCreditsSceneController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HauntedCreditsSceneController : MonoBehaviour {
+
+    
+    GameObject[] credits;
+    int totalCredits;
+    
+    int i = 0;
+
+    IEnumerator LerpThat(float speed, GameObject obj)
+    {
+        
+        Debug.Log("i: " + i);
+        float ratio = 0;
+        while (ratio < 1)
+        {
+            ratio += Time.deltaTime * speed;
+            Color col = obj.GetComponent<Renderer>().material.color;
+            col.a = Mathf.Lerp( 0f, 1, ratio);
+            obj.GetComponent<Renderer>().material.color = col;
+            yield return null;
+        }
+        
+         ratio = 0;
+        while (ratio < 1)
+        {
+            ratio += Time.deltaTime * speed;
+            Color col = obj.GetComponent<Renderer>().material.color;
+            col.a = Mathf.Lerp(col.a, 0f, ratio);
+            obj.GetComponent<Renderer>().material.color = col;
+            yield return null;
+        }
+        i--;
+        if (i >= 0)
+        {
+            StartCoroutine(LerpThat(.2f, credits[i]));
+        }
+       
+    }
+
+    // Use this for initialization
+    void Start () {
+        credits = GameObject.FindGameObjectsWithTag("credit");
+        totalCredits = credits.Length;
+        
+        foreach (GameObject c in credits)
+        {
+            Color col = c.GetComponent<Renderer>().material.color;
+            col.a = 0f;
+            c.GetComponent<Renderer>().material.color = col;
+            
+        }
+        i = totalCredits-1;
+        StartCoroutine(LerpThat(.2f,credits[i]));
+	}
+	
+}
---
+++ Assets/cameraPosition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraPosition : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	 target = GameObject.Find("TargetCameraPosition");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position;
+	}
+}
