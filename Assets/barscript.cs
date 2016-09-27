cd2e8da8f65e7c166c83cdc159eeaa084f223097
Travis Cornelius <travis@mybringback.com>
2016-09-27 12:58:16 -0500

    updated

--- Assets/barscript.cs
+++ Assets/barscript.cs
@@ -2,18 +2,25 @@
 using System.Collections;
 
 public class barscript : MonoBehaviour {
+    private AudioSource source;
+    private bool isTrue = true;
+    GameObject[] bars;
 
-	// Use this for initialization
 	void Start () {
-            
-	
+        source = GetComponent<AudioSource>();
+        bars = GetComponents<GameObject>();
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
-	    if (grimAnimator.stopped)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0,0,0), 1f * Time.deltaTime);
-        }
+        //  if (grimAnimator.stopped && isTrue)
+       //    {
+            foreach (GameObject bar in bars) {
+                 bar.transform.position = Vector3.MoveTowards(bar.transform.position, new Vector3(bar.transform.position.x, 2, bar.transform.position.z), 10f * Time.deltaTime);
+            }
+            isTrue = false;
+            source.Play();
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x,2,transform.position.z), 10f * Time.deltaTime);
+     //   }
 	}
 }
